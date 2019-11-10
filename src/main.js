1b68d4ce89cefd899000e6cbe5b3391280523fee
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-10 21:22:24 +0000

    add basic vuex store

--- src/main.js
+++ src/main.js
@@ -2,9 +2,25 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import Vuex from 'vuex'
 import '~/styles/main.scss'
 
-export default function (Vue, { router, head, isClient }) {
+export default function (Vue, { appOptions }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.use(Vuex)
+
+  appOptions.store = new Vuex.Store({
+    state: {
+      cart: []
+    },
+    mutations: {
+      addToCart: (state, newItem) => {
+        const itemExists = state.cart.find(item => item.id === newItem.id)
+
+        if (itemExists) itemExists.qty += newItem.qty
+        else state.cart.push(newItem)
+      }
+    }
+  })
 }
