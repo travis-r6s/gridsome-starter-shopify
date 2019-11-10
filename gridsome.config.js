89e4de24272c87c2a21c07a274c68bea0cd90d73
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-10 21:29:25 +0000

    use gridsome env prefix

--- gridsome.config.js
+++ gridsome.config.js
@@ -12,11 +12,11 @@ module.exports = {
     {
       use: '@gridsome/source-graphql',
       options: {
-        url: `https://${process.env.SHOPIFY_STOREFRONT}.myshopify.com/api/2019-07/graphql.json`,
+        url: `https://${process.env.GRIDSOME_SHOPIFY_STOREFRONT}.myshopify.com/api/2019-07/graphql.json`,
         fieldName: 'shopify',
         typeName: 'shopifyTypes',
         headers: {
-          'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_STOREFRONT_TOKEN
+          'X-Shopify-Storefront-Access-Token': process.env.GRIDSOME_SHOPIFY_STOREFRONT_TOKEN
         }
       }
     }
