a9762b74d30583106afa3328e8641f3f59d80708
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-10 20:21:06 +0000

    use bulma

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,10 @@
 <template>
   <Layout>
-
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    <g-image
+      alt="Example image"
+      src="~/favicon.png"
+      width="135" />
 
     <h1>Hello, world!</h1>
 
@@ -11,10 +13,15 @@
     </p>
 
     <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+      <a
+        href="https://gridsome.org/docs/"
+        target="_blank"
+        rel="noopener">Gridsome Docs</a>
+      <a
+        href="https://github.com/gridsome/gridsome"
+        target="_blank"
+        rel="noopener">GitHub</a>
     </p>
-
   </Layout>
 </template>
 
