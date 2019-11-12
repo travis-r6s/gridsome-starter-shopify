5e0ea47dafcaa34c0c862723a56e839e9745944e
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-12 13:42:41 +0000

    update homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,42 @@
 <template>
   <Layout>
-    <div class="hero">
-      <div class="hero-body">
-        <div class="container">
-          <h3 class="title">
-            Products
-          </h3>
+    <div class="hero is-large">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-two-fifths header">
+            <div class="header-content">
+              <h1 class="title is-size-1 is-family-secondary">
+                Our Core Collection
+              </h1>
+              <div
+                v-html="collection.descriptionHtml"
+                class="content" />
+              <g-link
+                :to="`collection/${collection.handle}`"
+                class="button">
+                Shop Now
+              </g-link>
+            </div>
+          </div>
+          <div class="column is-three-fifths">
+            <figure class="image">
+              <img
+                :src="collection.image.src"
+                :alt="collection.image.altText">
+            </figure>
+          </div>
         </div>
       </div>
     </div>
-    <div class="container">
+    <br>
+    <br>
+    <div class="container has-text-centered">
+      <hr>
+      <h3 class="title is-size-4 is-family-secondary">
+        Featured Products
+      </h3>
+      <hr>
+      <br>
       <div class="columns is-multiline">
         <div
           v-for="({ node: product }) in $page.shopify.products.edges"
@@ -23,10 +50,10 @@
                   :alt="product.images.edges[0].node.altText || product.title">
               </figure>
             </div>
-            <div class="card-content">
+            <div class="card-content has-text-left">
               <div class="media">
                 <div class="media-content">
-                  <p class="title is-4">
+                  <p class="title is-4 is-family-secondary">
                     {{ product.title }}
                   </p>
                   <p class="subtitle is-6">
@@ -55,10 +82,40 @@
   </Layout>
 </template>
 
+<script>
+export default {
+  metaInfo: {
+    title: 'Come, shop!'
+  },
+  computed: {
+    collection () { return this.$page.shopify.collections.edges[ 0 ].node }
+  },
+  methods: {
+    formatCurrency ({ currencyCode, amount }) {
+      return new Intl.NumberFormat('en-GB', { style: 'currency', currency: currencyCode }).format(amount)
+    }
+  }
+}
+</script>
+
 <page-query>
 query ShopifyProducts {
   shopify {
-    products (first: 30) {
+    collections (first: 1) {
+      edges {
+        node {
+          id
+          handle
+          title
+          descriptionHtml
+          image {
+            altText
+            src: transformedSrc(maxWidth: 800, maxHeight: 800, crop: CENTER)
+          }
+        }
+      }
+    }
+    products (first: 6) {
       edges {
         node {
           id
@@ -87,15 +144,12 @@ query ShopifyProducts {
 }
 </page-query>
 
-<script>
-export default {
-  metaInfo: {
-    title: 'Come, shop!'
-  },
-  methods: {
-    formatCurrency ({ currencyCode, amount }) {
-      return new Intl.NumberFormat('en-GB', { style: 'currency', currency: currencyCode }).format(amount)
-    }
+<style lang="scss" scoped>
+.header {
+  align-items: center;
+  display: flex;
+  .header-content {
+    text-align: center;
   }
 }
-</script>
+</style>
