8218b318c2a6b6518b8f320fcfe737aa523130e1
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-12 12:27:11 +0000

    add custom fonts

--- src/main.js
+++ src/main.js
@@ -1,32 +1,47 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+// Layouts
 import DefaultLayout from '~/layouts/Default.vue'
-import Vuex from 'vuex'
-import '~/styles/main.scss'
 
-import ApolloClient from 'apollo-boost'
+// Plugins
+import Vuex from 'vuex'
 import VueApollo from 'vue-apollo'
+
+// Dependencies
+import ApolloClient from 'apollo-boost'
 import fetch from 'isomorphic-fetch'
+
+// Styles
+import '~/styles/main.scss'
+import 'typeface-karla'
+import 'typeface-prata'
 
 export default function (Vue, { appOptions }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  // Import global plugins
   Vue.use(Vuex)
   Vue.use(VueApollo)
 
-  const apolloClient = new ApolloClient( {
+  // Create Apollo client
+  const apolloClient = new ApolloClient({
     fetch,
     uri: `https://${process.env.GRIDSOME_SHOPIFY_STOREFRONT}.myshopify.com/api/2019-07/graphql.json`,
     headers: {
       'X-Shopify-Storefront-Access-Token': process.env.GRIDSOME_SHOPIFY_STOREFRONT_TOKEN
     }
   })
+
+  // Add client to vue-apollo provider
   const apolloProvider = new VueApollo({
     defaultClient: apolloClient
   })
 
+  // Add provider to vue app
   appOptions.apolloProvider = apolloProvider
+
+  // Create Vuex store
   appOptions.store = new Vuex.Store({
     state: {
       cart: []
