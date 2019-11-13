15a36531706a1233c9465394444d3cced276f421
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-13 16:53:53 +0000

    generate from source types

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,15 +1,25 @@
 module.exports = {
   siteName: 'Gridsome + Shopify 😍',
+  templates: {
+    ShopifyProduct: [
+      {
+        path: '/product/:handle',
+        component: './src/templates/Product.vue'
+      }
+    ],
+    ShopifyCollection: [
+      {
+        path: '/collection/:handle',
+        component: './src/templates/Collection.vue'
+      }
+    ]
+  },
   plugins: [
     {
-      use: '@gridsome/source-graphql',
+      use: 'gridsome-source-shopify',
       options: {
-        url: `https://${process.env.GRIDSOME_SHOPIFY_STOREFRONT}.myshopify.com/api/2019-07/graphql.json`,
-        fieldName: 'shopify',
-        typeName: 'shopifyTypes',
-        headers: {
-          'X-Shopify-Storefront-Access-Token': process.env.GRIDSOME_SHOPIFY_STOREFRONT_TOKEN
-        }
+        storeName: process.env.GRIDSOME_SHOPIFY_STOREFRONT,
+        storefrontToken: process.env.GRIDSOME_SHOPIFY_STOREFRONT_TOKEN
       }
     }
   ]
