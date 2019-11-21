343c70ee09d0bbdb8b9dc293371c54b91c3ed741
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-21 21:11:35 +0000

    use mixin

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -85,38 +85,11 @@
 </template>
 
 <script>
-import FlexSearch from 'flexsearch'
-
+import Search from 'gridsome-plugin-flexsearch/SearchMixin'
 export default {
-  data: () => ({
-    searchIndex: null,
-    searchTerm: ''
-  }),
+  mixins: [Search],
   computed: {
-    cart () { return this.$store.state.cart },
-    searchResults () {
-      const searchTerm = this.searchTerm
-      if (!this.searchIndex || searchTerm.length < 3) return []
-      return this.searchIndex.search({ query: searchTerm, limit: 5 })
-    }
-  },
-  watch: {
-    $route (to, from) {
-      this.searchTerm = ''
-    }
-  },
-  async mounted () {
-    const { searchFields, index } = await fetch('/search.json').then(r => r.json())
-    this.searchIndex = new FlexSearch({
-      tokenize: 'strict',
-      depth: 3,
-      workers: 2,
-      doc: {
-        id: 'id',
-        field: searchFields
-      }
-    })
-    this.searchIndex.import(index, { serialize: false })
+    cart () { return this.$store.state.cart }
   }
 }
 </script>
